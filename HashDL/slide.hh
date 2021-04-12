6bd9e1a993a9dffe5c79458807a75b1a8da722b9
Yamada <ymd_h@outlook.jp>
2021-04-13 07:53:30 +0900

    Refactoring: activation -> data

--- HashDL/slide.hh
+++ HashDL/slide.hh
@@ -138,7 +138,7 @@ namespace HashDL {
   class Neuron {
   private:
     std::vector<int> is_active;
-    std::vector<data_t> activation;
+    std::vector<data_t> data;
     std::vector<data_t> gradient;
     std::vector<data_t> weight;
     data_t bias;
@@ -146,7 +146,7 @@ namespace HashDL {
     Neuron(): Neuron{16};
     Neuron(std::size_t prev_units,
 	   std::function<data_t()> weight_initializer = [](){ return 0; })
-      : is_active{}, activation{}, gradient{}, weight{}, bias{}
+      : is_active{}, data{}, gradient{}, weight{}, bias{}
     {
       weight.reserve(prev_units);
       std::generate_n(std::back_inserter(weight), prev_units, weight_initializer);
@@ -161,8 +161,8 @@ namespace HashDL {
       is_active.clear();
       is_active.resize(batch_size, 0);
 
-      activation.clear();
-      activation.resize(batch_size, 0);
+      data.clear();
+      data.resize(batch_size, 0);
 
       gradient.clear();
       gradient.resize(batch_size, 0);
@@ -176,12 +176,12 @@ namespace HashDL {
       if(is_active[batch_i]){
 
 	for(std::size_t j=0, data_size=X.size(); j<data_size; ++j){
-	  activation[batch_i] += weights[j]*data[j];
+	  data[batch_i] += weights[j]*data[j];
 	}
-	activation[batch_i] += bias;
+	data[batch_i] += bias;
       }
 
-      return activation[batch_i];
+      return data[batch_i];
     }
 
     const auto& get_weight() const noexcept { return weight; }
