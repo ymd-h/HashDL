1d3acf75bdd24b659c06fc2b4b974b23dea83059
Yamada <ymd_h@outlook.jp>
2021-04-13 08:25:56 +0900

    Add const specifier

--- HashDL/Activation.hh
+++ HashDL/Activation.hh
@@ -4,20 +4,20 @@
 namespace HashDL {
   template<typename T> class Activation {
   public:
-    virtual T call(T x) = 0;
-    virtual T back(T x,T dn_dy) = 0;
+    virtual T call(T x) const = 0;
+    virtual T back(T x,T dn_dy) const = 0;
   };
 
   template<typename T> class Linear : public Activation<T> {
   public:
-    virtual T call(T x) override noexcept { return x; }
-    virtual T back(T x, T dn_dy) override noexcept { return dn_dy; }
+    virtual T call(T x) override const noexcept { return x; }
+    virtual T back(T x, T dn_dy) override const noexcept { return dn_dy; }
   };
 
   template<typename T> class ReLU : public Activation<T> {
   public:
-    virtual T call(T x) override noexcept { return (x>0)? x: 0; }
-    virtual T back(T x, T dn_dy) override noexcept {  return (x>0)? dn_dy: 0; }
+    virtual T call(T x) override const noexcept { return (x>0)? x: 0; }
+    virtual T back(T x, T dn_dy) override const noexcept {  return (x>0)? dn_dy: 0; }
   };
 }
 
