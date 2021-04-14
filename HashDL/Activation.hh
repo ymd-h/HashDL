21ce611af120e4158fb334854c1a33111aa36a86
Yamada <ymd_h@outlook.jp>
2021-04-14 20:36:25 +0900

    Add space

--- HashDL/Activation.hh
+++ HashDL/Activation.hh
@@ -5,7 +5,7 @@ namespace HashDL {
   template<typename T> class Activation {
   public:
     virtual T call(T x) const = 0;
-    virtual T back(T x,T dn_dy) const = 0;
+    virtual T back(T x, T dn_dy) const = 0;
   };
 
   template<typename T> class Linear : public Activation<T> {
