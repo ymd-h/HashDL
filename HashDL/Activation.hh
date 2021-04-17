e35adb2954e96131fb0aa306fd66e1d38f153d23
Yamada <ymd_h@outlook.jp>
2021-04-17 12:51:31 +0900

    Change API tp use y instead of x

--- HashDL/Activation.hh
+++ HashDL/Activation.hh
@@ -5,19 +5,19 @@ namespace HashDL {
   template<typename T> class Activation {
   public:
     virtual T call(T x) const = 0;
-    virtual T back(T x, T dn_dy) const = 0;
+    virtual T back(T y, T dn_dy) const = 0;
   };
 
   template<typename T> class Linear : public Activation<T> {
   public:
     virtual T call(T x) override const noexcept { return x; }
-    virtual T back(T x, T dn_dy) override const noexcept { return dn_dy; }
+    virtual T back(T y, T dn_dy) override const noexcept { return dn_dy; }
   };
 
   template<typename T> class ReLU : public Activation<T> {
   public:
     virtual T call(T x) override const noexcept { return (x>0)? x: 0; }
-    virtual T back(T x, T dn_dy) override const noexcept {  return (x>0)? 1: 0; }
+    virtual T back(T y, T dn_dy) override const noexcept {  return (y>0)? 1: 0; }
   };
 }
 
