77751263c432ade405659af05316d50c48f119d5
Yamada <ymd_h@outlook.jp>
2021-04-17 12:56:22 +0900

    Refactoring: dn_dy -> dL_dy

--- HashDL/Activation.hh
+++ HashDL/Activation.hh
@@ -7,25 +7,25 @@ namespace HashDL {
   template<typename T> class Activation {
   public:
     virtual T call(T x) const = 0;
-    virtual T back(T y, T dn_dy) const = 0;
+    virtual T back(T y, T dL_dy) const = 0;
   };
 
   template<typename T> class Linear : public Activation<T> {
   public:
     virtual T call(T x) override const { return x; }
-    virtual T back(T y, T dn_dy) override const { return dn_dy; }
+    virtual T back(T y, T dL_dy) override const { return dL_dy; }
   };
 
   template<typename T> class ReLU : public Activation<T> {
   public:
     virtual T call(T x) override const { return (x>0)? x: 0; }
-    virtual T back(T y, T dn_dy) override const {  return (y>0)? 1: 0; }
+    virtual T back(T y, T dL_dy) override const {  return (y>0)? 1: 0; }
   };
 
   template<typename T> class Sigmoid : public Activation<T> {
   public:
     virtual T call(T x) override const { return 1.0/(1.0 + std::exp(-x)); }
-    virtual T back(T y, T dn_dy) override const { return y*(1-y); }
+    virtual T back(T y, T dL_dy) override const { return y*(1-y); }
   }
 }
 
