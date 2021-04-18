2cbccdbd1dce6d21da3f3afd44de146817f50253
Yamada <ymd_h@outlook.jp>
2021-04-19 07:39:15 +0900

    Add python class

--- HashDL/hashdl.pyx
+++ HashDL/hashdl.pyx
@@ -3,18 +3,28 @@
 
 from HashDL cimport slide
 
+cdef class Optimizer:
+    cdef slide.Optimizer* opt
 
-cdef class SGD:
-    def __cinit__(self, *args, **kwargs):
-        pass
 
-cdef class Adam:
-    def __cinit__(self, *args, **kwargs):
-        pass
+cdef class SGD(Optimizer):
+    def __cinit__(self, rl=1e-4, decay=1.0, *args, **kwargs):
+        self.opt = (slide.Optimizer*) = new slide.SGD(rl, decay)
+
+
+cdef class Adam(Optimizer):
+    def __cinit__(self, rl=1e-4, *args, **kwargs):
+        self.opt = (slide.Optimizer*) = new slide.Adam(rl)
+
 
 cdef class Network:
-    def __cinit__(self, *args, **kwargs):
-        pass
+    cdef slide.Network* net
+    def __cinit__(self, input_size, units=(30, 30, 30),
+                  optimizer = None, *args, **kwargs):
+        optimizer = optimizer or Adam()
+
+        self.net = new slide.Network()
+
 
     def __call__(self, X):
         pass
