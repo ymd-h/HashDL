1b691d39d613e1b97d6ccb4c9e909c02c7bf1f51
Yamada <ymd_h@outlook.jp>
2021-04-25 21:18:11 +0900

    Remove bias

--- HashDL/slide.hh
+++ HashDL/slide.hh
@@ -77,8 +77,7 @@ namespace HashDL {
     Neuron(): Neuron{16};
     Neuron(std::size_t prev_units,
 	   std::function<T()> weight_initializer = [](){ return 0; })
-      : data{}, gradient{}, weight{prev_units}, bias{}
-    {}
+      : data{}, gradient{}, weight{prev_units} {}
     Neuron(const Neuron&) = default;
     Neuron(Neuron&&) = default;
     Neuron& operator=(const Neuron&) = default;
