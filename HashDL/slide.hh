1b1cdbf7031e870e2267af586245097c985f5eb4
Yamada <ymd_h@outlook.jp>
2021-04-30 08:48:42 +0900

    Fix: Call get() to get pointer

--- HashDL/slide.hh
+++ HashDL/slide.hh
@@ -362,7 +362,7 @@ namespace HashDL {
       std::for_each(std::execution::par, batch_idx.begin(), batch_idx.end(),
 		    [&, this](auto i){
 		      auto d = Data<T>{dL_dy.begin(i), dL_dy.end(i)};
-		      this->layer.back()->backward(i, d);
+		      this->layer.back()->get()->backward(i, d);
 		    });
 
       opt->step();
