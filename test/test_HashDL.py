11cf5da1bb5c4aed461366e7b7dab996aa86383e
Yamada <ymd_h@outlook.jp>
2021-05-01 21:41:57 +0900

    Add unit test

--- test/test_HashDL.py
+++ test/test_HashDL.py
@@ -112,7 +112,6 @@ class TestNetwork(unittest.TestCase):
     def test_default_network(self):
         net = HashDL.Network(2)
 
-
     def test_negetive_intput_size(self):
         with self.assertRaises(ValueError):
             net = HashDL.Network(-2)
@@ -121,11 +120,35 @@ class TestNetwork(unittest.TestCase):
         with self.assertRaises(TypeError):
             net = HashDL.Network("abc")
 
-
     def test_invalid_type_None(self):
         with self.assertRaises(TypeError):
             net = HashDL.Network(None)
 
+    def test_negative_units(self):
+        with self.assertRaises(ValueError):
+            net = HashDL.Network(2, [-10, 2, 5])
+
+    def test_negative_L(self):
+        with self.assertRaises(ValueError):
+            net = HashDL.Network(2, (10, 10), -3)
+
+    def test_SGD(self):
+        net = HashDL.Network(4, optimizer=HashDL.SGD())
+
+    def test_Adam(self):
+        net = HashDL.Network(4, optimizer=HashDL.Adam())
+
+    def test_WTA(self):
+        net = HashDL.Network(4, hash=HashDL.WTA(8, 10))
+
+    def test_DWTA(self):
+        net = HashDL.Network(4, hash=HashDL.DWTA(8, 10))
+
+    def test_ConstantFrequency(self):
+        net = HashDL.Network(4, scheduler=HashDL.ConstantFrequency(50))
+
+    def test_ExponentialDecay(self):
+        net = HashDL.Network(4, scheduler=HashDL.ExponentialDecay(50, 1e-3))
 
 
 if __name__ == "__main__":
