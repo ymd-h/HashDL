6d85fd4271f7ab0b014416de3216c6419f99ac44
Yamada <ymd_h@outlook.jp>
2021-05-03 14:42:04 +0900

    Fix: Multiply dL/dy

--- HashDL/activation.hh
+++ HashDL/activation.hh
@@ -20,19 +20,19 @@ namespace HashDL {
   template<typename T> class Linear : public Activation<T> {
   public:
     T call(T x) const override { return x; }
-    T back(T y, T dL_dy) const override { return 1; }
+    T back(T y, T dL_dy) const override { return dL_dy; }
   };
 
   template<typename T> class ReLU : public Activation<T> {
   public:
     T call(T x) const override { return (x>0)? x: 0; }
-    T back(T y, T dL_dy) const override {  return (y>0)? 1: 0; }
+    T back(T y, T dL_dy) const override {  return (y>0)? dL_dy: 0; }
   };
 
   template<typename T> class Sigmoid : public Activation<T> {
   public:
     T call(T x) const override { return 1.0/(1.0 + std::exp(-x)); }
-    T back(T y, T dL_dy) const override { return y*(1-y); }
+    T back(T y, T dL_dy) const override { return y*(1-y)*dL_dy; }
   };
 }
 
