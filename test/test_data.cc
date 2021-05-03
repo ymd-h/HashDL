7bae9721f0459f921f1f0834b7ce39c7ae175620
Yamada <ymd_h@outlook.jp>
2021-05-03 23:37:20 +0900

    Add vector construction

--- test/test_data.cc
+++ test/test_data.cc
@@ -4,21 +4,20 @@
 
 int main(int argc, char** argv){
   auto test = Test{};
+  auto v = std::vector<float>{0.1, 0.2, 0.3, 0.4, 0.5};
 
   test.Add([](){
     auto data = Data<float>{10};
     AssertEqual(data.size(), 10);
   }, "Data.size()");
 
-  test.Add([](){
-    auto v = std::vector<float>{0.1, 0.2, 0.3, 0.4, 0.5};
+  test.Add([=](){
     auto data = Data<float>(v.begin(), v.end());
 
     AssertEqual(data, v);
   }, "Data construction");
 
-  test.Add([](){
-    auto v = std::vector<float>{0.1, 0.2, 0.3, 0.4, 0.5};
+  test.Add([=](){
     auto data = Data<float>(v.begin(), v.end());
 
     AssertEqual(data[2], v[2]);
@@ -26,14 +25,19 @@ int main(int argc, char** argv){
     AssertEqual(data[6], v[6]);
   }, "Data access");
 
-  test.Add([](){
-    auto v = std::vector<float>{0.1, 0.2, 0.3, 0.4, 0.5};
+  test.Add([=](){
     auto data = Data<float>(v.begin(), v.end(), [](auto vi){ return 2*vi; });
 
     AssertEqual(data[2], v[2] * 2);
     AssertEqual(data[4], v[4] * 2);
     AssertEqual(data[6], v[6] * 2);
   }, "Data transform");
+
+  test.Add([=](){
+    auto data = Data<float>(v);
+
+    AssertEqual(data, v);
+  }, "Data vector construction");
 
   return test.Run();
 }
