a865e9b4b740e62813236ee59d20f780331cd7f3
Yamada <ymd_h@outlook.jp>
2021-05-04 11:37:27 +0900

    Add Absolute torelance

--- test/unittest.hh
+++ test/unittest.hh
@@ -124,7 +124,7 @@ inline constexpr void AssertEqual(L&& lhs, R&& rhs){
 				      (std::is_floating_point_v<RR> ?
 				       std::numeric_limits<RR>::epsilon() : RR{0}));
 
-    not_equal = !(abs(lhs - rhs) <= eps * std::max<LR>(abs(lhs), abs(rhs)));
+    not_equal = !(abs(lhs - rhs) <= eps * std::max<LR>(1.0, abs(lhs), abs(rhs)));
   } else {
     not_equal = !Equal(lhs, rhs);
   }
