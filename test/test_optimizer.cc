df18536008458878200ea88f5afce7bb97bb9a3f
Yamada <ymd_h@outlook.jp>
2021-05-05 22:25:58 +0900

    Add SGD Client unit test

--- test/test_optimizer.cc
+++ test/test_optimizer.cc
@@ -28,7 +28,45 @@ int main(int argc, char** argv){
     AssertEqual(sgd.eta(), eta * decay * decay);
   }, "SGD with decay");
 
+  test.Add([](){
+    auto rl = 0.1;
+    auto sgd = SGD<float>{rl};
 
+    auto c = sgd.client();
+
+    AssertEqual(c->diff(0), c->diff(0));
+    AssertEqual(c->diff(0.5) * 2, c->diff(1.0));
+
+    AssertEqual(c->diff(2.0), (sgd.step(), c->diff(2.0)));
+
+    AssertEqual(c->diff(0.5), - rl * 0.5);
+
+    if(c){
+      delete c;
+      c = nullptr;
+    }
+  }, "SGD Client");
+
+  test.Add([](){
+    auto rl = 0.1;
+    auto decay = 0.01;
+    auto sgd = SGD<float>{rl, decay};
+
+    auto c = sgd.client();
+
+    AssertEqual(c->diff(0.7), c->diff(0.7));
+    AssertEqual(c->diff(0.9), - rl * 0.9);
+
+    auto eta = c->diff(2.3);
+    sgd.step();
+
+    AssertEqual(c->diff(9.9), eta * decay);
+
+    if(c){
+      delete c;
+      c = nullptr;
+    }
+  }, "SGD Client with decay");
 
   return test.Run();
 }
