4357c4e6d5e63d23dd07eb31137d0a1b5c855bf1
Yamada <ymd_h@outlook.jp>
2021-05-09 17:09:05 +0900

    Exponential Decay Frequency

--- test/test_scheduler.cc
+++ test/test_scheduler.cc
@@ -24,5 +24,33 @@ int main(int argc, char** argv){
     AssertTrue(confw());
   }, "2-step Constant Frequency");
 
+  test.Add([](){
+    auto exp = ExponentialDecay<float>{1, 0};
+
+    AssertTrue(exp());
+    AssertTrue(exp());
+    AssertTrue(exp());
+    AssertTrue(exp());
+  }, "Exp 0-decay");
+
+  test.Add([](){
+    auto exp = ExponentialDecay<float>{1, 100};
+
+    AssertTrue(exp());
+    AssertFalse(exp());
+    AssertFalse(exp());
+    AssertFalse(exp());
+  }, "Exp large-decay");
+
+  test.Add([](){
+    auto exp = ExponentialDecay<float>{1, 1};
+
+    AssertTrue(exp());
+    AssertFalse(exp());
+    AssertFalse(exp());
+    AssertFalse(exp());
+    AssertTrue(exp());
+  }, "Exp 1-decay");
+
   return Test.Run();
 }
