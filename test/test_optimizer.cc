673935a6d894bfcf309870e63631902e4e96ce78
Yamada <ymd_h@outlook.jp>
2021-05-09 11:01:00 +0900

    Use parenthis for template constructor

--- test/test_optimizer.cc
+++ test/test_optimizer.cc
@@ -126,7 +126,7 @@ int main(int argc, char** argv){
     AssertTrue(c);
 
     auto x = 0.7;
-    AssertRaises<std::runtime_error>{[=](){ c->diff(x); }};
+    AssertRaises<std::runtime_error>([=](){ c->diff(x); });
 
     c->step();
     AssertEqual(c->diff(x), c->diff(x));
