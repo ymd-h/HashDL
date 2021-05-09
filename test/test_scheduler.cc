70f41ee1fb11bf584a8550f4c9bbecd4f3d06291
Yamada <ymd_h@outlook.jp>
2021-05-09 16:35:58 +0900

    Add unit test for Constant Frequency

---
+++ test/test_scheduler.cc
@@ -0,0 +1,27 @@
+#include <scheduler.hh>
+
+#include "unittest.hh"
+
+int main(int argc, char** argv){
+  auto test = Test{};
+
+  test.Add([](){
+    auto confw = ConstantFrequency{};
+
+    AssertTrue(confw());
+    AssertTrue(confw());
+    AssertTrue(confw());
+    AssertTrue(confw());
+  }, "1-step Constant Frequency");
+
+  test.Add([](){
+    auto confw = ConstantFrequency{2};
+
+    AssertFalse(confw());
+    AssertTrue(confw());
+    AssertFalse(confw());
+    AssertTrue(confw());
+  }, "2-step Constant Frequency");
+
+  return Test.Run();
+}
