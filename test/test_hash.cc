ad6a78c53c12fd284aa6d792d00531b180da7e5c
Yamada <ymd_h@outlook.jp>
2021-05-10 07:43:19 +0900

    DWTA error unit test

--- test/test_hash.cc
+++ test/test_hash.cc
@@ -36,5 +36,18 @@ int main(int argc, char** argv){
     AssertEqual(dwta.encode(d), hashcode_t{0});
   }, "DWTA");
 
+  test.Add([](){
+    using DWTA_t = DWTA<float>;
+    using Assert_t = AssertRaises<std::runtime_error>;
+
+    Assert_t([](){ DWTA_t{8, 2, 10}; }, "data size < sample size");
+    Assert_t([](){ DWTA_t{64, 16, 8}; }, "bin size * sample bits > 64");
+    Assert_t([=](){
+      auto dwta = DWTA_t{8, 8, 4};
+      auto d = Data<float>{16};
+      dwta.encode(d);
+    }, "Mis much data size");
+  }, "DWTA error");
+
   return test.Run();
 }
