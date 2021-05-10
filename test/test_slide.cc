e0319420c1c3309650bc1386040c3f15a842096c
Yamada <ymd_h@outlook.jp>
2021-05-11 07:46:25 +0900

    Unit Test for Param

--- test/test_slide.cc
+++ test/test_slide.cc
@@ -7,5 +7,18 @@ int main(int argc, char** argv){
 
   auto test = Test{};
 
+  test.Add([](){
+    auto opt = std::unique_ptr<Optimizer<float>>(new SGD<float>{});
+    auto p = Param<float>{opt};
+
+    AssertEqual(p(), 0);
+
+    p.add_grad(0.5);
+    AssertEqual(p(), 0);
+
+    p.update();
+    AssertEqual(p(), 0.5);
+  }, "Param");
+
   return test.Run();
 }
