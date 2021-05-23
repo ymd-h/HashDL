582014444caac5b4c3ee89d4246bf1a846dc0173
Yamada <ymd_h@outlook.jp>
2021-05-23 17:09:36 +0900

    Add no hidden network test

--- test/test_slide.cc
+++ test/test_slide.cc
@@ -312,5 +312,12 @@ int main(int, char**){
     AssertEqual(Net(x1), x1);
   }, "Network with activation");
 
+  test.Add([&](){
+    auto Net = Network<float>(1, std::vector<std::size_t>{}, 10, &wta, opt, sch);
+    auto x = std::vector<float>{0};
+    auto x1 = BatchView<float>{1, 1, x.data()};
+    AssertEqual(Net(x1), x1);
+  }, "No hidden network");
+
   return test.Run();
 }
