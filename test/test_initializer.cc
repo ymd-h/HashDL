45476b4cc0eb5173246d4964dd438a8f09990fbe
Yamada <ymd_h@outlook.jp>
2021-05-24 12:38:07 +0900

    Add initializer test

--- test/test_initializer.cc
+++ test/test_initializer.cc
@@ -5,5 +5,17 @@
 int main(int, char**){
   auto test = Test{};
 
+  test.Add([](){
+    auto init = ConstantInitializer<float>{0};
+    AssertEqual(init(), 0);
+    AssertEqual(init(), 0);
+  }, "Constant Initializer");
+
+  test.Add([](){
+    auto init = ConstantInitializer<float>{0.5};
+    AssertEqual(init(), 0.5);
+    AssertEqual(init(), 0.5);
+  }, "Constant Initializer with non-zero");
+
   return test.Run();
 }
