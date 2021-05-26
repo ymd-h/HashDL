6a09f00cee11be92e6b2383577fdc2610a965248
Yamada <ymd_h@outlook.jp>
2021-05-26 12:34:06 +0900

    Fix: Export Activation/Initializer

--- HashDL/__init__.py
+++ HashDL/__init__.py
@@ -1,4 +1,6 @@
 from .hashdl import (SGD, Adam,
                      WTA, DWTA,
                      ConstantFrequency, ExponentialDecay,
+                     Lineear, ReLU, Sigmoid,
+                     ConstantInitializer, GaussInitializer,
                      Network)
