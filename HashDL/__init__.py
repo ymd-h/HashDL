150c843ef8ac619ef2e09af3afea3b41f984af76
Yamada <ymd_h@outlook.jp>
2021-05-26 12:39:18 +0900

    Fix: typo

--- HashDL/__init__.py
+++ HashDL/__init__.py
@@ -1,6 +1,6 @@
 from .hashdl import (SGD, Adam,
                      WTA, DWTA,
                      ConstantFrequency, ExponentialDecay,
-                     Lineear, ReLU, Sigmoid,
+                     Linear, ReLU, Sigmoid,
                      ConstantInitializer, GaussInitializer,
                      Network)
